3b98e5710f1bf49bc639413cf851fee2137b18ee
Lajob <ustym.hanyk@ukr.net>
2023-05-21 18:11:48 +0300

    Added docs, excluded display_mode enum into separate module from lamp projector

--- models/abstract_projector.py
+++ models/abstract_projector.py
@@ -2,9 +2,20 @@ from abc import ABC, abstractmethod
 
 
 class AbstractProjector(ABC):
-    """AbstractProjector class
+    """
+    AbstractProjector class
 
     This class represents an abstract projector with common properties for all projectors.
+
+    Attributes:
+        model (str): The model of the projector.
+        resolution (str): The resolution of the projector.
+        connected_device (str): The currently connected device.
+
+    Methods:
+        add_input_device: Adds an input device to the projector.
+        disconnect_device: Disconnects the current input device.
+        get_remaining_working_hours: Abstract method to get the remaining working hours of the projector.
     """
 
     def __init__(self, model: str = None, resolution: str = None, connected_device: str = None):
@@ -13,13 +24,28 @@ class AbstractProjector(ABC):
         self.connected_device = connected_device
 
     def add_input_device(self, device: str):
+        """
+        Adds an input device to the projector.
+
+        Args:
+            device (str): The input device to be added.
+        """
         self.connected_device = device
 
     def disconnect_device(self):
+        """
+        Disconnects the current input device.
+        """
         self.connected_device = None
 
     @abstractmethod
     def get_remaining_working_hours(self) -> int:
+        """
+        Abstract method to get the remaining working hours of the projector.
+
+        Returns:
+            int: The remaining working hours of the projector.
+        """
         pass
 
     def __str__(self):
--- models/lamp_projector.py
+++ models/lamp_projector.py
@@ -1,17 +1,24 @@
-from abstract_projector import AbstractProjector
-
-from enum import Enum
-
-
-class DisplayMode(Enum):
-    SPORT = 1
-    ACTIVE = 2
-    PRESENTATION = 3
+from models.abstract_projector import AbstractProjector
+from models.display_mode import DisplayMode
 
 
 class LampProjector(AbstractProjector):
     """
-    Represents a LampProjector device
+    LampProjector class
+
+    This class represents a lamp-based projector device.
+
+    Attributes:
+        model (str): The model of the lamp projector.
+        resolution (str): The resolution of the projector.
+        connected_device (str): The currently connected device.
+        lamp_hours (int): The total lamp hours used.
+        mode (str): The display mode of the projector.
+        max_lamp_hours (int): The maximum lamp hours supported by the projector.
+
+    Methods:
+        increase_lamp_hours: Increases the lamp hours of the projector.
+        get_remaining_working_hours: Gets the remaining lamp hours of the projector.
     """
 
     def __init__(self, model=None, resolution=None, connected_device=None, lamp_hours=0, mode='Active',
@@ -22,11 +29,23 @@ class LampProjector(AbstractProjector):
         self.max_lamp_hours = max_lamp_hours
 
     def increase_lamp_hours(self, hours):
+        """
+        Increases the lamp hours of the projector.
+
+        Args:
+            hours (int): The number of hours to increase the lamp hours.
+        """
         self.lamp_hours += hours
 
     def get_remaining_working_hours(self):
+        """
+        Gets the remaining lamp hours of the projector.
+
+        Returns:
+            int: The remaining lamp hours of the projector.
+        """
         return self.max_lamp_hours - self.lamp_hours
 
     def __str__(self):
-        return f"LampProjector({super().__str__()}, lamp_hours={self.lamp_hours}, mode={self.mode}," \
-               f" max_lamp_hours={self.max_lamp_hours})"
+        return f"LampProjector({super().__str__()}, lamp_hours={self.lamp_hours}, mode={self.mode}, " \
+               f"max_lamp_hours={self.max_lamp_hours})"
